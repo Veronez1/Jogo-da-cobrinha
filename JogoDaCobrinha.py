ce91497733590f15d671a2fa4e5be702b39b7a1d
Davi Veronez <87334467+Veronez1@users.noreply.github.com>
2022-10-17 20:09:43 -0300

    Update JogoDaCobrinha.py
    Remoção das linhas verticais

--- JogoDaCobrinha.py
+++ JogoDaCobrinha.py
@@ -91,10 +91,6 @@ while not game_over:
     screen.fill((0,0,0))
     screen.blit(apple, apple_pos)
     
-    for x in range(0, 600, 10): # Desenhe linhas verticais
-        pygame.draw.line(screen, (40, 40, 40), (x, 0), (x, 600))
-    for y in range(0, 600, 10): # Desenhe linhas verticais
-        pygame.draw.line(screen, (40, 40, 40), (0, y), (600, y))
     
     score_font = font.render('Score: %s' % (score), True, (255, 255, 255))
     score_rect = score_font.get_rect()
